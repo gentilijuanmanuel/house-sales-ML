8c9e50988ef672fab65b19150ecf94ced9f46c71
Juan Manuel Gentili <gentilijuanmanuel80974a@gmail.com>
2018-03-17 10:53:13 -0300

    Initial Commit

---
+++ house-sales-coreML/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  house-sales-coreML
+//
+//  Created by Juan Manuel Gentili on 17/3/18.
+//  Copyright © 2018 Juan Manuel Gentili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
