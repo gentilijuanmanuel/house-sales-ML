a46e188657f8e352d4498dd01af9a251ac841620
Juan Manuel Gentili <gentilijuanmanuel80974a@gmail.com>
2018-03-18 00:01:21 -0300

    First commit

--- house-sales-coreML/ViewController.swift
+++ house-sales-coreML/ViewController.swift
@@ -9,17 +9,61 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var bedrooms: Int = 0
+    var bathrooms: Int = 0
+    var size: Int = 0
+    
+    @IBOutlet weak var bedroomsLabel: UILabel!
+    @IBOutlet weak var bathroomsLabel: UILabel!
+    @IBOutlet weak var sizeLabel: UILabel!
+    
+    @IBOutlet weak var predictButton: UIButton!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBOutlet weak var bedroomsSlider: UISlider!
+    @IBOutlet weak var bathroomsSlider: UISlider!
+    @IBOutlet weak var sizeSlider: UISlider!
+    
+    @IBAction func changeValue(_ sender: UISlider) {
+        setChanges()
+    }
+    
+    
+    @IBAction func predict(_ sender: UIButton) {
+        let price = predictPriceOfHouse(bathrooms: Double(bathrooms), bedrooms: Double(bedrooms), size: Double(size))
+        if price > 0 {
+            resultLabel.text = "El precio rondará los USD \(Int(price))."
+        } else {
+            resultLabel.text = "El sistema no pudo predecir buenos resultados 😔"
+        }
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setChanges()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func predictPriceOfHouse(bathrooms: Double, bedrooms: Double, size: Double) -> Double {
+        let houseSales = HouseSalesInCalifornia()
+        guard let price = try? houseSales.prediction(Bedrooms: bedrooms, Bathrooms: bathrooms, Size: size) else {
+            print("Se produjo un error.")
+            return 0.0
+        }
+        print(price.Price)
+        return price.Price
     }
-
-
+    
+    func setChanges() {
+        predictButton.layer.cornerRadius = 5
+        bedrooms = Int(bedroomsSlider.value)
+        bathrooms = Int(bathroomsSlider.value)
+        size = Int(sizeSlider.value)
+        
+        bedroomsLabel.text = "\(bedrooms) habitaciones"
+        bathroomsLabel.text = "\(bathrooms) baños"
+        sizeLabel.text = "\(size) pies cuadrados"
+    }
 }
 
